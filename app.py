1ea267d083ff5a3ac4cc5ef6ebefc033cdd43600
Jonathan <jonathan.ortiz07@gmail.com>
2019-11-15 15:08:15 -0500

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLACLHEMY_TRACK_MODIFACTIONS'] = False
 app.secret_key = 'jonathan'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
